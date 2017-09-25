e5df0bb87574926515952c672409495044bc9a8b
Chae Kim <Chae Kim>
2017-09-25 16:45:18 -0400

    WIP files

---
+++ assets/javascript/trivia_game.js
@@ -0,0 +1,18 @@
+
+
+		$(document).ready(function() {
+
+			// Add SessionInterval timer
+			// Declare variables
+			// Create Radio Buttons for each question
+			// Create submit answers button
+			// Add JQuery event handlers to identify
+					// Radio button choices
+					// On-click for Submit answers
+					// Add all HMTL responses to Array
+			// For loop through all Array ansswers
+					// If right, add to Correct++
+					// If wrong, add to Incorrect++
+			// Display results
+
+});
